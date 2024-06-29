53076daff0cb37778bd68afadd0576f9486d32c0
MiguelSalvati <miguelsalvati@gmail.com>
2024-06-29 18:23:37 -0300

    agregamos CORS

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from app.database import init_app
 from app.views import *
+from flask_cors import CORS
 
 #crear una instancia de flask
 app = Flask(__name__)
 
 init_app(app) 
+
+#permita solicitudes desde cualquier origen
+CORS(app)
 
 #asociacion de rutas con vistas
 app.route('/usuarios',methods=['GET'])(get_all_usuarios)
