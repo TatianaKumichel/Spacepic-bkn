8f9bcd3fc989fa71d774aa206883984255537330
tati <rominatatianakumichel@gmail.com>
2024-06-29 14:51:47 -0300

    creacion de front para gestion de usuarios y enlace con back incompleto(muestra lista de usuarios de bd)

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from app.database import init_app
 from app.views import *
+from flask_cors import CORS
 
 #crear una instancia de flask
 app = Flask(__name__)
 
 init_app(app) 
-
+#permitir solicitudes desde cualquier origen
+CORS(app)
 #asociacion de rutas con vistas
 app.route('/usuarios',methods=['GET'])(get_all_usuarios)
 app.route('/usuarios',methods=['POST'])(create_usuario)
